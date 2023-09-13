279b4610b2641f9f36870fb5572637100ff631a9
Alexander Pilatuña <imvert1998@gmail.com>
2023-09-12 23:23:47 -0500

    Update vite.config.ts
    upercase the first letter of my nickname

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 //configuracion importate incluir base
 export default defineConfig({
-  base: "https://imvert.github.io/Countries-of-the-world/",
+  base: "https://Imvert.github.io/Countries-of-the-world/",
   plugins: [react()],
 })
